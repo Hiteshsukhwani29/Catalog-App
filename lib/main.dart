015055ff6f38d03b4ec56670fc2cc9c71b8ee351
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-01-11 22:53:36 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Catalog App"),
+          ),
+        ),
+      ),
+    );
+  }
+}
